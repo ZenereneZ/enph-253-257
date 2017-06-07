e118b0148bbfc3108f41ce556864a532243e35bf
engphys101\p253 <yunief@hotmail.com>
2017-06-07 15:47:55 -0700

    257 - updated read6TMPs to hae dynamic plot view

--- enph-257/MATLAB/enph257_read6TMPs.m
+++ enph-257/MATLAB/enph257_read6TMPs.m
@@ -1,7 +1,7 @@
 %connects to an arduino and reads all 6 analog inputs, displaying it and
 %writing it to a file
 clear a;
-a = arduino();  %port number hard-coded to Yuni's laptop
+a = arduino();
 
 dateTime = datestr(datetime('now'));
 dateTime = strrep(dateTime,':','-');
@@ -9,7 +9,8 @@ dateTime = strrep(dateTime,' ','_');
 fileName = [pwd '/output_files/' dateTime '.csv']; %use this for windows
 %fileName = [pwd '\output_files\' dateTime '.csv']; %use this mac
 
-vals = zeros(1,6);    %array of values to append to file
+vals = zeros(1,6);      %array of values to append to file
+plotMat = [];           %matrix used to plot realtime data
 
 while true
     tic;
@@ -17,8 +18,11 @@ while true
         pin = strcat('A', int2str(i - 1));    %name of the pin to read from
         vals(i) = readVoltage(a, pin);
     end
-    disp(vals); %write value to command line output
     dlmwrite(fileName, vals, '-append'); %append to csv file
+    disp(vals); %write value to command line output
+    plotMat = [plotMat ; vals]; %append readings
+    plot(plotMat);
+    legend('A0','A1','A2','A3','A4','A5');
     
     %if less than 1 second has elapsed, wait 1 second
     %if more than 1 second has elapsed, just move on
