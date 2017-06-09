a6abb9272c5510d764a6b813b1d9d58af2c4b1a5
Dylan Lu <dyl055a@gmail.com>
2017-06-09 14:46:49 -0700

    commented and cleaned up a couple of things

--- enph-257/Python/CoolingRod.py
+++ enph-257/Python/CoolingRod.py
@@ -1,60 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt = 0.1
-simSensors = [[] for i in np.arange(6)]
-k = 230
-L = 0.3
-a = 0.01
-c = 900
-roh = 2700
-dx = 0.01
-P = 9.5
-kc = 11
-Tamb = 20
-sigma = 5.67*10**(-8)
-epsilon = 1
-colors = ['y','m','c','r','g','b']
-CToK = 273.15
 
-T_n = np.full((30), 27.5)
+### STUFF WE CAN CHANGE ###
+epsilon = 1 # emissivity (0-1)
+kc = 11 # conduction constant
+efficiency = 1 # efficiency of power (0-1)
+
+
+### DO NOT CHANGE ###
+dt = 0.1 # delta time (s)
+k = 205 # thermal conductivity (W/(m*k))
+L = 0.3 # length of rod (m)
+a = 0.01 # radius (m)
+c = 900 # specific heat capacity
+roh = 2700 # density (kg/m^3)
+dx = 0.01 # delta x (m)
+P = 9.5 # max power (W)
+Tamb = 24 # ambient temperature (C)
+sigma = 5.67*10**(-8) # stefan-boltzmann constant (W/(m^2 * k**4))
+CToK = 273.15 # conversion from Celcius to Kelvin
+seconds_of_simulation = 3000 # 50 minutes
+colors = ['y','m','c','r','g','b']
+
+
+### DEPENDANT VARIABLES
+numSteps = seconds_of_simulation/dt
+simSensors = np.zeros((seconds_of_simulation, 6))
+T_n = np.full((30), Tamb, dtype=np.float32)
+
+
 def run_sim():
     global P
-    for i in np.arange(3000/dt):
-        if (int(i/(300/dt)) % 2 == 1):
-            P = 0
+    power = P * efficiency
+    for i in np.arange(numSteps):
+        if (int(i/(numSteps)) % 2 == 1):
+            power = 0
         else:
-            P = 9.5
-        T_n[0] += delta_temp_first(T_n[0], T_n[1])
+            power = P * efficiency
+        T_n[0] += delta_temp_first(T_n[0], T_n[1], power)
         T_n[-1] += delta_temp_last(T_n[-2], T_n[-1])
         for j in range(1, 29):
             T_n[j] += delta_temp_middle(T_n[j-1], T_n[j], T_n[j+1])
 
         if (i % (1/dt) == 0):
-            simSensors[0].append(T_n[1])
-            simSensors[1].append((T_n[6]+T_n[7])/2)
-            simSensors[2].append(T_n[12])
-            simSensors[3].append((T_n[17]+T_n[18])/2)
-            simSensors[4].append(T_n[23])
-            simSensors[5].append((T_n[28]+T_n[29])/2)
+            idt = int(i * dt)
+            simSensors[idt, 0] = (T_n[1])
+            simSensors[idt, 1] = (T_n[6]+T_n[7])/2
+            simSensors[idt, 2] = (T_n[12])
+            simSensors[idt, 3] = (T_n[17]+T_n[18])/2
+            simSensors[idt, 4] = (T_n[23])
 
 ### Common radiation ###
 def delta_temp_radiative(t):
     return -(2/(c*roh*a))*dt*(kc*(t-Tamb)+epsilon*sigma*((t+CToK)**4-(Tamb+CToK)**4))
 
 ### FIRST NODE ###
-def delta_temp_first(t0, t1):
+def delta_temp_first(t0, t1, power):
     convective = delta_temp_first_convective(t0)
     conductive = delta_temp_first_conductive(t0, t1)
-    power_in = delta_temp_first_power()
+    power_in = delta_temp_first_power(power)
     radiative = delta_temp_radiative(t0)
     return convective + conductive + power_in# + radiative
 def delta_temp_first_convective(t0):
     return -2*kc*(t0 - Tamb)*dt/(c*roh*a)
 def delta_temp_first_conductive(t0, t1):
     return -k*(t0 - t1)*dt/(c*roh*dx**2)
-def delta_temp_first_power():
-    return P*dt/(c*roh*np.pi*(a**2)*dx)
+def delta_temp_first_power(power):
+    return power*dt/(c*roh*np.pi*(a**2)*dx)
 
 ### MIDDLE NODES ###
 def delta_temp_middle(t0, t1, t2):
@@ -99,10 +112,10 @@ for i in arange(6):
 def main():
     run_sim()
     plt.figure()
-    t = range(1, 3001)
+    t = range(1, seconds_of_simulation + 1)
     for i in range(6):
-        plt.scatter(t, simSensors[i], c = colors[i], marker = 'x', linewidths = 0.01)
-    plt.xlim([0, 3000])
+        plt.scatter(t, simSensors[:, i], c = colors[i], marker = 'x', linewidths = 0.01)
+    plt.xlim([0, seconds_of_simulation])
     plt.ylim([0, 100])
     plt.title('Temperature vs. Time')
     plt.show()
