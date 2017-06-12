d8eb54a626d1f2f90c98701ee40c7c42df53ab35
Dylan Lu <dyl055a@gmail.com>
2017-06-12 15:24:50 -0700

    data looking better

--- enph-257/Python/CoolingRod.py
+++ enph-257/Python/CoolingRod.py
@@ -1,40 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import csv
 
 ### STUFF WE CAN CHANGE ###
 epsilon = 1 # emissivity (0-1)
-kc = 11 # conduction constant
-efficiency = 1 # efficiency of power (0-1)
-
+kc = 10 # conduction constant
+efficiency = 0.9 # efficiency of power (0-1)
+c = 1700 # specific heat capacity
+Tamb = 24 # ambient temperature (C)
+k = 240 # thermal conductivity (W/(m*k))
 
 ### DO NOT CHANGE ###
 dt = 0.1 # delta time (s)
-k = 205 # thermal conductivity (W/(m*k))
-L = 0.3 # length of rod (m)
+L = 0.305 # length of rod (m)
 a = 0.01 # radius (m)
-c = 900 # specific heat capacity
-roh = 2700 # density (kg/m^3)
+rho = 2700 # density (kg/m^3)
 dx = 0.01 # delta x (m)
 P = 9.5 # max power (W)
-Tamb = 24 # ambient temperature (C)
 sigma = 5.67*10**(-8) # stefan-boltzmann constant (W/(m^2 * k**4))
 CToK = 273.15 # conversion from Celcius to Kelvin
 seconds_of_simulation = 3000 # 50 minutes
-colors = ['y','m','c','r','g','b']
+colors = ['b','g','r','c','m','y']
 
-
-### DEPENDANT VARIABLES
+### DEPENDANT VARIABLES ###
 numSteps = seconds_of_simulation/dt
-simSensors = np.zeros((seconds_of_simulation, 6))
+#simSensors = np.empty((seconds_of_simulation, 6), dtype = np.float16)
+simSensors = [[] for i in np.arange(6)]
 T_n = np.full((30), Tamb, dtype=np.float32)
-
 
 def run_sim():
     global P
     power = P * efficiency
     for i in np.arange(numSteps):
-        if (int(i/(numSteps)) % 2 == 1):
+        if (int((i)/(numSteps *dt)) % 2 == 1):
             power = 0
         else:
             power = P * efficiency
@@ -44,16 +42,26 @@ def run_sim():
             T_n[j] += delta_temp_middle(T_n[j-1], T_n[j], T_n[j+1])
 
         if (i % (1/dt) == 0):
+            '''
             idt = int(i * dt)
-            simSensors[idt, 0] = (T_n[1])
-            simSensors[idt, 1] = (T_n[6]+T_n[7])/2
-            simSensors[idt, 2] = (T_n[12])
-            simSensors[idt, 3] = (T_n[17]+T_n[18])/2
-            simSensors[idt, 4] = (T_n[23])
+            simSensors[idt, 0] = (T_n[1] + T_n[2])/2
+            simSensors[idt, 1] = (T_n[7])
+            simSensors[idt, 2] = (T_n[12] + T_n[13])/2
+            simSensors[idt, 3] = (T_n[18])
+            simSensors[idt, 4] = (T_n[23] + T_n[24])/2
+            simSensors[idt, 5] = (T_n[29])
+            '''
+            simSensors[0].append((T_n[1] + T_n[2])/2)
+            simSensors[1].append(T_n[7])
+            simSensors[2].append((T_n[12] + T_n[13])/2)
+            simSensors[3].append(T_n[18])
+            simSensors[4].append((T_n[23] + T_n[24])/2)
+            simSensors[5].append(T_n[29])
 
-### Common radiation ###
+
+### Common radiation for all nodes ###
 def delta_temp_radiative(t):
-    return -(2/(c*roh*a))*dt*(kc*(t-Tamb)+epsilon*sigma*((t+CToK)**4-(Tamb+CToK)**4))
+    return -(2/(c*rho*a))*dt*(kc*(t-Tamb)+epsilon*sigma*((t+CToK)**4-(Tamb+CToK)**4))
 
 ### FIRST NODE ###
 def delta_temp_first(t0, t1, power):
@@ -61,13 +69,13 @@ def delta_temp_first(t0, t1, power):
     conductive = delta_temp_first_conductive(t0, t1)
     power_in = delta_temp_first_power(power)
     radiative = delta_temp_radiative(t0)
-    return convective + conductive + power_in# + radiative
+    return convective + conductive + power_in + radiative
 def delta_temp_first_convective(t0):
-    return -2*kc*(t0 - Tamb)*dt/(c*roh*a)
+    return -2*kc*(t0 - Tamb)*dt/(c*rho*a)
 def delta_temp_first_conductive(t0, t1):
-    return -k*(t0 - t1)*dt/(c*roh*dx**2)
+    return -k*(t0 - t1)*dt/(c*rho*dx**2)
 def delta_temp_first_power(power):
-    return power*dt/(c*roh*np.pi*(a**2)*dx)
+    return power*dt/(c*rho*np.pi*(a**2)*dx)
 
 ### MIDDLE NODES ###
 def delta_temp_middle(t0, t1, t2):
@@ -75,7 +83,7 @@ def delta_temp_middle(t0, t1, t2):
     radiative = delta_temp_radiative(t1)
     return conductive + radiative
 def delta_temp_middle_conductive(t0, t1, t2):
-    return (k/(c*roh))*dt*(t0-2*t1+t2)/(dx**2)
+    return (k/(c*rho))*dt*(t0-2*t1+t2)/(dx**2)
 
 ### LAST NODE ###
 def delta_temp_last(t99, t100):
@@ -84,40 +92,48 @@ def delta_temp_last(t99, t100):
     radiative = delta_temp_radiative(t100)
     return convective + conductive + radiative
 def delta_temp_last_convective(t100):
-    return -2*kc*(t100 - Tamb)*dt/(c*roh*a)
+    return -2*kc*(t100 - Tamb)*dt/(c*rho*a)
 def delta_temp_last_conductive(t99, t100):
-    return k*(t99 - t100)*dt/(c*roh*dx**2)
-
-
-'''
-sensors = [[] for i in np.arange(6)]
-
-import csv
-with open('june5_1-41.csv', 'r') as file:
-    dataReader = csv.reader(file, delimiter = ',')
-    #stores data in a list of 6 lists(one for each sensor)
-    for row in dataReader:
-        j = 0
-        for i in arange(6):
-            sensors[j].append(float(row[i]))
-            j += 1
-file.close()
-
-t = arange(1, len(sensors[0]) + 1)
-
-for i in arange(6):
-    plot(t, [j*100/7.67 for j in sensors[i]])
-'''
+    return k*(t99 - t100)*dt/(c*rho*dx**2)
 
 def main():
     run_sim()
     plt.figure()
     t = range(1, seconds_of_simulation + 1)
     for i in range(6):
-        plt.scatter(t, simSensors[:, i], c = colors[i], marker = 'x', linewidths = 0.01)
+        #plt.plot(t, simSensors[:, i])
+        plt.plot(t, simSensors[i])
     plt.xlim([0, seconds_of_simulation])
-    plt.ylim([0, 100])
+    plt.ylim([20, 60])
     plt.title('Temperature vs. Time')
+
+
+    actual_sensors = [[] for i in range(6)]
+    with open('../MATLAB/output_files/june5_1-41.csv', 'r') as file:
+        dataReader = csv.reader(file, delimiter = ',')
+        #stores data in a list of 6 lists(one for each sensor)
+        for row in dataReader:
+            for i in range(6):
+                actual_sensors[i].append(float(row[i]))
+
+
+
+    actual_sensors[0] = np.add(np.multiply(actual_sensors[0], 12.35), 2.138)
+    actual_sensors[1] = np.add(np.multiply(actual_sensors[1], 12.566), 1.5609)
+    actual_sensors[2] = np.add(np.multiply(actual_sensors[2], 11.4814), 2.38055)
+    actual_sensors[3] = np.add(np.multiply(actual_sensors[3], 11.9304), 2.82118)
+    actual_sensors[4] = np.add(np.multiply(actual_sensors[4], 12.06), 2.888)
+    actual_sensors[5] = np.add(np.multiply(actual_sensors[5], 11.94229), 2.8868)
+
+    for i in range(6):
+        factor = 50
+        cumsum_vec = np.cumsum(np.insert(actual_sensors[i], 0, 0))
+        ma_vec = (cumsum_vec[factor:] - cumsum_vec[:-factor]) / factor
+        moving_average = ma_vec[::factor]
+        t = np.arange(1, len(moving_average) + 1)
+        t = np.multiply(t, factor)
+        plt.scatter(t, moving_average,c=colors[i], marker = '.', linewidths = 0.1)
+
     plt.show()
 
 
